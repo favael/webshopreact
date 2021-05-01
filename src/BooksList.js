61d0fe373daec2596129dfb594acf91e8368f320
Favael <rafaldogoda86@gmail.com>
2021-05-01 20:34:57 +0200

    added working component

--- src/BooksList.js
+++ src/BooksList.js
@@ -1,17 +1,43 @@
 import React from 'react';
 
 class BooksList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {booksList: []};
+    }
 
 componentDidMount() {
-    fetch('https://favael-webshop.herokuapp.com/book')
-    .then(response => {
-        console.log(response);
+    fetch('http://localhost:8080/book')
+    .then(response => response.json())
+    .then(jsonResponse=> {
+        this.setState({booksList: jsonResponse});
+    })
+}
+
+renderList = () => {
+  return  this.state.booksList.map((book, index) => {
+        return <li key = {index}>
+            <p>Kategoria: {book.booksCategory}</p>
+            <p>Tytuł: {book.title}</p>
+            <p>Opis: {book.description}</p>
+            <p>Autor: {book.author}</p>
+            <p>Cena: {book.prize}</p>
+            <iframe width="1020" height="615"
+            src = {book.url}>
+            </iframe>
+
+        </li>
     })
 }
 
     render() {
         return(
+            <div>
             <h1>Books list!</h1>
+            <ul>
+                {this.renderList()}
+            </ul>
+            </div>
         )
     }
 }
